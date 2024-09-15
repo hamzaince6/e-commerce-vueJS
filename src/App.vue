91689ac32458808caaf801e73e0fa56b51c01d1f
Hamza İnce <hamzaince001@gmail.com>
2024-09-15 12:23:09 +0300

    continue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-    <ProductComparison/>
+    <ShopCard/>
 </template>
 
 <script setup>
 // import HomePage from '/src/components/HomePage.vue'
 // import ShopList from "@/components/ShopList.vue";
 // import SingleProduct from "@/components/SingleProduct.vue";
-import ProductComparison from "@/components/ProductComparison.vue";
+// import ProductComparison from "@/components/ProductComparison.vue";
+import ShopCard from "@/components/ShopCard.vue";
 </script>
--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,7 +1,8 @@
 <template>
-    <AppHeader />
-    <ShopListHeader :title="pageTitle" />
-    <AppFooter />
+  <AppHeader />
+  <ShopListHeader :title="pageTitle" />
+  <ShopListBanner/>
+  <AppFooter />
 </template>
 
 <script>
@@ -9,12 +10,19 @@ import { defineComponent } from 'vue';
 import AppHeader from '@/components/global/AppHeader.vue';
 import AppFooter from '@/components/global/AppFooter.vue';
 import ShopListHeader from '@/components/sections/ShopListHeader.vue';
+import ShopListBanner from "@/components/sections/ShopListBanner.vue";
 
 export default defineComponent({
-  components: {ShopListHeader, AppFooter, AppHeader},
+  name: 'ShopCard',
+  components: {
+    ShopListBanner,
+    AppHeader,
+    ShopListHeader,
+    AppFooter
+  },
   data() {
     return {
-      pageTitle: 'Card',
+      pageTitle: 'Cart',
     };
   }
 });
